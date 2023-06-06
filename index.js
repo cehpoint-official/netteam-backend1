7ce9c75c92cb59a7efb6d59db3956f0240ea4ea0
Divya <karan.boss1000@gmail.com>
2023-06-06 16:21:13 +0530

    initial

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const { v4: uuidv4 } = require("uuid");
+require("dotenv").config();
 
 const app = express();
 const server = http.createServer(app);
@@ -80,7 +81,6 @@ io.on("connection", (socket) => {
 });
 
 // Start the server
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
